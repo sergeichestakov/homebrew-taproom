4d27639cc9f4ee317332e65d96405f7dae21736d
Sergei Chestakov <schestakov@ucdavis.edu>
2019-07-27 15:23:49 -0700

    Update sha256 for new gzip

--- todo-to-issue.rb
+++ todo-to-issue.rb
@@ -3,7 +3,7 @@ class TodoToIssue < Formula
   homepage "https://github.com/sergeichestakov/todo-to-issue"
   url "https://github.com/sergeichestakov/todo-to-issue/releases/download/v0.1.0/todo-to-issue-0.1-tar.gz"
   version "0.1.0"
-  sha256 "11fddd3de504804c991c375db8c2eb3f9e149e653c67d064bdba48aedbd779a0"
+  sha256 "608fb5e98906622f203813260378532e6beefceb70f94a343bf309f6e42c85ed"
 
   def install
     bin.install "todo-to-issue"
