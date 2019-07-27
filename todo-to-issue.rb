f518e8672f4a39048280b9a2cd1d9cabd9f916d4
Sergei Chestakov <schestakov@ucdavis.edu>
2019-07-27 15:44:11 -0700

    Update url to use raw file directly from GH repo

--- todo-to-issue.rb
+++ todo-to-issue.rb
@@ -1,7 +1,7 @@
 class TodoToIssue < Formula
   desc "CLI tool that converts TODO comments into GitHub issues"
   homepage "https://github.com/sergeichestakov/todo-to-issue"
-  url "https://github.com/sergeichestakov/todo-to-issue/releases/download/v0.1.0/todo-to-issue-0.1-tar.gz"
+  url "https://github.com/sergeichestakov/todo-to-issue/raw/master/dist/todo-to-issue-0.1-tar.gz"
   version "0.1.0"
   sha256 "608fb5e98906622f203813260378532e6beefceb70f94a343bf309f6e42c85ed"
 
