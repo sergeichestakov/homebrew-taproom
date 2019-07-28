db5f0610dd9cebc481c233c32baf4c6b0cdf2546
Sergei Chestakov <schestakov@ucdavis.edu>
2019-07-28 13:59:56 -0700

    Bump version to 0.1.1 and update sha256

--- todo-to-issue.rb
+++ todo-to-issue.rb
@@ -1,9 +1,9 @@
 class TodoToIssue < Formula
   desc "CLI tool that converts TODO comments into GitHub issues"
   homepage "https://github.com/sergeichestakov/todo-to-issue"
-  url "https://github.com/sergeichestakov/todo-to-issue/raw/master/dist/todo-to-issue-0.1-tar.gz"
-  version "0.1.0"
-  sha256 "608fb5e98906622f203813260378532e6beefceb70f94a343bf309f6e42c85ed"
+  url "https://github.com/sergeichestakov/todo-to-issue/raw/master/dist/todo-to-issue-0.1.tar.gz"
+  version "0.1.1"
+  sha256 "e5226325c32fa30bdc62beac27427d73ca12e69183ce156e7e29c2c46405315f"
 
   def install
     bin.install "todo-to-issue"
